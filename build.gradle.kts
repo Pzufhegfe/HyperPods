ef9dd6e59c5382084b7ae4040249b99d3f5bd82e
Pzufhegfe <87434326+Pzufhegfe@users.noreply.github.com>
2026-04-10 13:25:23 +0800

    Disable JetBrains Compose plugin in build.gradle.kts
    Comment out the JetBrains Compose plugin application.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlin) apply false
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.jetbrains.compose) apply false
+//    alias(libs.plugins.jetbrains.compose) apply false
 }
 
 tasks.register<Delete>("clean") {
